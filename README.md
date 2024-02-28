7ae1a8096042c76e28cdd840b7d7ccad02323419
gabo4481 <143552501+gabo4481@users.noreply.github.com>
2024-02-28 15:10:14 -0400

    cambio css y js final

--- README.md
+++ README.md
@@ -4,7 +4,3 @@
 - espacios para texto de ingreso de datos y muestra de datos
 - boton encriptar y desencriptar y copiar a portapapeles
 - responsividad (mejorar)
-
-redes sociales:
-https://www.linkedin.com/in/gabriel-mendoza-devolver/
-https://github.com/gabo4481
--- encriptador.js
+++ encriptador.js
@@ -54,21 +54,65 @@ function sacartextocaja(){
 }
 
 function textoEncriptado(frase){
-    let fraseEncriptada = frase.replace(/e/img,"enter");
-    fraseEncriptada = fraseEncriptada.replace(/i/img,"imes");
-    fraseEncriptada = fraseEncriptada.replace(/a/img,"ai");
-    fraseEncriptada = fraseEncriptada.replace(/o/img,"ober");
-    fraseEncriptada = fraseEncriptada.replace(/u/img,"ufat");
-    return fraseEncriptada;
+    var texto = frase;
+    var textoFinal = "";
+
+    for(var i = 0; i < texto.length; i++){
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "ai"
+        }
+        else if(texto[i] == "e"){
+            textoFinal = textoFinal + "enter"
+        }
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "imes"
+        }
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "ober"
+        }
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "ufat"
+        }
+        else{
+            textoFinal = textoFinal + texto[i]
+        }
+    }
+    return textoFinal;
 }
 
 function textoDesencriptado(frase){
-    let fraseDesencriptada = frase.replace(/enter/img,"e");
-    fraseDesencriptada = fraseDesencriptada.replace(/imes/img,"i");
-    fraseDesencriptada = fraseDesencriptada.replace(/ai/img,"a");
-    fraseDesencriptada = fraseDesencriptada.replace(/ober/img,"o");
-    fraseDesencriptada = fraseDesencriptada.replace(/ufat/img,"u");
-    return fraseDesencriptada;
+    var texto = frase;
+    var textoFinal = "";
+
+    for(var i = 0; i < texto.length; i++){
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "a"
+            i = i+1;
+        }
+        else if(texto[i] == "e"){
+            textoFinal = textoFinal + "e"
+            i = i+4;
+        }
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "i"
+            i = i+3;
+        }
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "o"
+            i = i+3;
+        }
+        
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "u"
+            i = i+3;
+        }
+        else{
+            textoFinal = textoFinal + texto[i];
+        }
+        
+    }
+
+    return textoFinal;
 }
 
 const botonCopiar = document.querySelector(".boton-copiar");
